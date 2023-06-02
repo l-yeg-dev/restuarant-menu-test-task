49dca3609ebbc82ff7782fe1c27365037b0a654d
l-yeg-dev <l.yeg.dev@gmail.com>
2023-06-02 14:11:13 +0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+  <Order />
+</template>
+
+<script>
+import Order from "./components/Order/Order.vue";
+import Header from "./components/Header/Header.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Order,
+  }
+}
+</script>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row bg-dark text-white justify-content-center py-3">
+    <div class="col-auto">
+      <a class="d-flex align-items-center text-decoration-none text-white" href="#">
+        <img src="../../assets/hamburger.png" alt="logo">
+        <span class="ms-3 fw-bold">Y O L O</span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+
+}
+</script>
