354b7ac769f9327726fa0c2f48664d398ad8a4ec
l-yeg-dev <l.yeg.dev@gmail.com>
2023-06-05 11:11:32 +0400

    add what is left

--- src/components/Order/Order.vue
+++ src/components/Order/Order.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="order container-fluid">
     <div class="order__wrapper row">
-          <div class="col-6 offset-3">
+          <div class="col-md-6 offset-md-3">
             <div class="text-dark d-flex justify-content-center mb-5">
               <Logo />
             </div>
             <Multiselect
                 v-model="order"
                 mode="tags"
-                placeholder="Select employees"
+                placeholder=""
                 label="name"
+                track-by="name"
+                searchable
                 :close-on-select="false"
                 :search="true"
                 :options="options"
-                @select="handleSelect"
+                :object="true"
                 :clearOnSelect="false"
-                :hide-selected="false"
-                :canClear="false"
-                :allowAbsent="true"
-                searchable
+                :hide-selected="selectingIngredient"
+                :canClear="true"
+                :allow-absent="true"
+                @select="handleSelect"
+                @clear="resetOptions"
             >
-              <template v-slot:tag="{ option, handleTagRemove, disabled }">
+              <template v-slot:tag="{ option, disabled }">
                 <div class="multiselect-tag is-user px-1">
                   {{ option.name }}
+                  <span
+                      v-if="!disabled"
+                      class="multiselect-tag-remove"
+                      @mousedown.prevent="handleTagRemove({...option})"
+                  >
+                    <span class="multiselect-tag-remove-icon"></span>
+                  </span>
                 </div>
               </template>
 
@@ -39,13 +49,14 @@
 
 import Logo from "../Logo/Logo.vue";
 import Multiselect from "@vueform/multiselect";
-import { getMenu } from "../../services/menuService";
+import {getMenu} from "../../services/menuService";
+import {v4 as uuidv4} from 'uuid';
 
 const menuItemsBase = [
-  "Menu-item",
-  "Ingredient",
-  "AND",
-  "Receipt"
+  {value: uuidv4(), name: "Menu-item"},
+  {value: uuidv4(), name: "Ingredient"},
+  {value: uuidv4(), name: "AND"},
+  {value: uuidv4(), name: "Receipt"},
 ]
 
 export default {
@@ -62,18 +73,26 @@ export default {
       }
 
       this.priceList = data['Menu-Item'];
-      this.menu = Object.keys(data['Menu-Item']).map(item => item);
-      this.ingredients = data.Ingredient;
+      this.menu = Object.keys(data['Menu-Item']).map(menuItem => {
+        const id = uuidv4();
+        this.ingredients[id] = data.Ingredient[menuItem].map(ingredient => ({value: uuidv4(), name: ingredient, parentId: id}))
+        return {
+          value: id,
+          id,
+          name: menuItem,
+        }
+      });
     })
   },
   data () {
     return {
-      initialMenu: [menuItemsBase[0]],
+      actionMenu: [],
       selectedAction: null,
       selectedDish: null,
       order: [],
       menu:[],
       ingredients: {},
+      selectedIngredients: [],
       isActionSelected: false,
       isDishSelected: false,
       isReceiptShown: false,
@@ -83,14 +102,24 @@ export default {
   },
   computed:{
     options() {
+      if (!this.order.length) {
+        return [menuItemsBase[0]];
+      }
       if (this.selectingAction) {
-        return this.initialMenu;
+        return this.actionMenu.map(action => ({...action}));
       }
       if (this.selectingDish){
-        return this.menu;
+        return this.menu.map(menu => {
+          menu.value = uuidv4();
+          return {...menu};
+        });
       }
       if (this.selectingIngredient) {
-        return this.ingredients[this.selectedDish];
+        return this.ingredients[this.selectedDish.id].map(ingredient => {
+          const isDisabled = !!this.selectedIngredients.find(item => ingredient.name === item.name.split("I/").pop())
+          ingredient.value = uuidv4();
+          return {...ingredient, disabled: isDisabled};
+        });
       }
       if (this.showingReceipt) {
         return this.receipt;
@@ -109,84 +138,132 @@ export default {
     showingReceipt() {
       return this.isReceiptShown;
     },
-    canSeeReceipt() {
-      return !!this.initialMenu.find(item=> item === menuItemsBase[3])
-    },
-    canSeeAnd() {
-      return !!this.initialMenu.find(item=> item === menuItemsBase[2])
-    }
   },
   methods:{
-    handleSelect(item) {
-      console.log('selectedItem', item);
-
-      debugger
-      if (item === menuItemsBase[3]){
-        this.showReceipt();
-        this.isDishSelected = true;
-        this.isReceiptShown = true;
+    handleSelect(option) {
+      if (option.value === menuItemsBase[3].value){
+        this.handleReceiptAction();
         return;
       }
 
-      if (item === menuItemsBase[2]) {
-        const lowerCasedAnd = menuItemsBase[2].toLowerCase()
-        this.order = this.order.map(orderItem => orderItem === item ? lowerCasedAnd : orderItem)
-        if (!this.selectingAction) {
-          this.isActionSelected = true;
-          this.isDishSelected = true;
-
-          return;
-        }
-        this.initialMenu = [menuItemsBase[0], menuItemsBase[1]]
-
+      if (option.value === menuItemsBase[2].value) {
+        this.handleAndAction();
         return;
       }
 
       if (this.selectingAction){
-        if (item === menuItemsBase[1]) {
-          this.removeFromOrder(item)
-          this.isActionSelected = true;
-          this.isDishSelected = true;
-
-          return;
-        }
-        this.isActionSelected = true;
-
+        this.handleActionSelect(option);
         return;
       }
 
       if (this.selectingDish) {
-        const orderDish = 'M/' + item;
-        this.removeFromOrder(item, this.initialMenu[0])
-        this.order.push(orderDish);
-        this.isDishSelected = true;
-        this.selectedDish = item;
-
+        this.selectedIngredients = [];
+        this.handleSelectingDish(option)
         return;
       }
 
       if (this.selectingIngredient) {
-        const orderIngredient = 'I/' + item;
-        this.removeFromOrder(item, this.initialMenu[0])
-        this.order.push(orderIngredient);
-        if (!this.canSeeReceipt) {
-          this.initialMenu.push('Receipt');
-        }
-        this.initialMenu = this.initialMenu.filter(menuItem => (menuItem !== menuItemsBase[0] && menuItem !== menuItemsBase[1]))
-        if (!this.canSeeAnd){
-          this.initialMenu.unshift(menuItemsBase[2])
-        }
-        this.isActionSelected = false;
-        this.isDishSelected = false;
-
+        this.handleSelectingIngredient(option);
       }
     },
+    handleReceiptAction() {
+      this.showReceipt();
+      this.isDishSelected = true;
+      this.isReceiptShown = true;
+      this.removeFromOrder(menuItemsBase[3]);
+    },
+    handleAndAction() {
+      if (!this.selectingAction) {
+        this.isActionSelected = true;
+        this.isDishSelected = true;
+
+        return;
+      }
+      this.actionMenu = [menuItemsBase[0], menuItemsBase[1]];
+    },
+
+    handleActionSelect(option) {
+
+      this.isActionSelected = true;
+
+      if (option.value === menuItemsBase[1].value) {
+        this.isDishSelected = true;
+      }
+
+    },
+
+    handleSelectingDish(option) {
+      this.removeFromOrder(menuItemsBase[0]);
+      this.order.at(-1).name = 'M/' +option.name;
+      this.isDishSelected = true;
+      this.selectedDish = option;
+    },
+
+    handleSelectingIngredient(option) {
+      this.removeFromOrder(menuItemsBase[0]);
+      this.removeFromOrder(menuItemsBase[1]);
+      this.order.at(-1).name = 'I/'+option.name;
+      menuItemsBase[2].value = uuidv4();
+      menuItemsBase[3].value = uuidv4();
+      this.actionMenu = [menuItemsBase[2], menuItemsBase[3]]
+      this.actionMenu = this.actionMenu.filter(menuItem => (
+          menuItem.value !== menuItemsBase[0].value && menuItem.value !== menuItemsBase[1].value
+      ));
+      this.isActionSelected = false;
+      this.isDishSelected = false;
+      this.selectedIngredients.push(option);
+    },
+
+    handleTagRemove(option) {
+      if (option?.name === menuItemsBase[2].name || option.id) {
+
+        let canDelete = false;
+        let deletingFor = null;
+        if (option.id) {
+          deletingFor = option;
+        }
+
+        this.order.forEach(orderItem => {
+          if (orderItem.value === option.value) {
+            canDelete = true;
+          }
+
+          if (option.id) {
+            if (orderItem.id && orderItem.value !== deletingFor.value) {
+              canDelete = false;
+            }
+          } else {
+            if (canDelete === true && orderItem.name === menuItemsBase[2].name && orderItem.value !== option.value) {
+              canDelete = false;
+            }
+          }
+
+          if (canDelete && orderItem.value !== option.value) {
+            this.removeFromOrder(orderItem);
+          }
+        });
+      }
+      this.removeFromOrder(option);
+
+
+        this.resetOptions();
+
+      if (!this.order.length) {
+        this.resetOptions();
+      }
+    },
+
+    resetOptions() {
+      this.isDishSelected=false;
+      this.isActionSelected = false
+    },
+
     showReceipt() {
       let billItems = {};
       let total = 0;
       this.order.forEach(orderItem => {
-        if (orderItem.includes('M/')) {
-          const orderItemName = orderItem.split("M/").pop();
+        if (orderItem.name.includes('M/')) {
+          const orderItemName = orderItem.name.split("M/").pop();
           const orderItemPrice = this.priceList[orderItemName];
           if(!billItems[orderItemName]) {
             billItems[orderItemName] = orderItemPrice;
@@ -197,13 +274,16 @@ export default {
         }
       });
       billItems.Total = total;
-      this.receipt = Object.keys(billItems).map(billItem => `${billItem} - ${billItems[billItem]}NIS`);
-      this.receipt.unshift("Order:");
+      this.receipt = Object.keys(billItems).map(billItem => ({
+        name: `${billItem} - ${billItems[billItem]}NIS`,
+        disabled: true
+      }));
+      this.receipt.unshift({name:"Order:", disabled: true});
     },
-    removeFromOrder(item, itemType = null) {
-      this.order = this.order.filter(orderItem => (orderItem !== item && orderItem !== itemType))
+    removeFromOrder(item) {
+      this.order = this.order.filter(orderItem => (orderItem.value !== item.value ));
     }
-  }
+  },
 }
 </script>
 <style src="@vueform/multiselect/themes/default.css"></style>
